9c2f2814bf2d00b87f8b7664822656dd39e09432
Samuel Fagbewesa <boluwatifefagbewesa@gmail.com>
2025-10-28 21:08:56 -0400

    Refactor netIncome class and add tax calculation

--- Project2/src/netIncome.java
+++ Project2/src/netIncome.java
@@ -1,2 +1,34 @@
-public class netIncome {
+import java.util.Scanner;
+
+public class Netincome{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter total revenue: ");
+        double totalRevenue = input.nextDouble();
+
+        System.out.println("Enter tax rate(%):");
+        double taxRate = input.nextDouble();
+
+        System.out.println("Are you Married Filing Jointly? (yes=1 / no=0):");
+        int marriedFilingJointly = input.nextInt();
+
+        double tax;
+        if(marriedFilingJointly == 1) {
+            tax = calculateMFJ(totalRevenue, taxRate);
+        }else{
+            tax = totalRevenue * (taxRate / 100);
+        }
+
+        double netIncome = totalRevenue - tax;
+
+        System.out.println("Tax: " + tax);
+        System.out.println("Net Income: " + netIncome);
+
+    }
+
+    public static double calculateMFJ(double totalRevenue, double taxRate){
+    return totalRevenue * (taxRate/100) *0.9;
+    }
 }
+
+
