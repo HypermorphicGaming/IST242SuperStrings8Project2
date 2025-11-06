11d16e29cc8f2043e80b9eeab0652f121172c914
khaliel <khalamyrie@gmail.com>
2025-11-06 16:33:59 -0500

    updated UserIO class

--- Project2/src/UserIO/UserIO.java
+++ Project2/src/UserIO/UserIO.java
@@ -1,5 +1,8 @@
 package UserIO;
+import java.util.Scanner;
 public class UserIO {
+
+    private static Scanner scnr = new Scanner(System.in);
 
     // prints output
     public void displayMessage(String message) {
@@ -8,18 +11,43 @@ public class UserIO {
 
     // displays prompt and returns user's number
     public double getDoubleInput(String prompt) {
-        System.out.println(prompt);
-    // add return statement
+        System.out.print(prompt + " ");
+        while (!scnr.hasNextDouble()) {
+            System.out.println("Invalid input. Please enter a number.");
+            scnr.next();
+        }
+        double input = scnr.nextDouble();
+        scnr.nextLine();
+        return input;
     }
 
     // displays prompt and returns user's text
     public String getStringInput(String prompt) {
         System.out.println(prompt);
-    // add return statement
+        return scnr.nextLine();
     }
 
     // displays menu options and returns user choice
     public int getMenuChoice(String[] options) {
+        System.out.println("Please choose an option:");
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ". " + options[i]);
+        }
 
+        int choice = 0;
+        while (true) {
+            System.out.println("Enter your choice (1-" + options.length + "): ");
+            if (scnr.hasNextInt()) {
+                choice = scnr.nextInt();
+                scnr.nextLine();
+                if (choice >= 1 && choice <= options.length) {
+                    break;
+                }
+        } else {
+                scnr.nextInt();
+            }
+        System.out.println("Invalid choice. Try again.");
+        }
+        return choice;
     }
 }
